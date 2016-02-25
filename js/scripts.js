3d99d329d2f5fc0d49aeeff542be70e18a15107a
genevieve latimer and kendra dunham <student@epicodus.com>
2016-02-24 20:56:49 -0800

    it

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,23 @@
 
-var equilateral = function(input) {
-  if (input % 3 === 0) {
-    return true;
+var triangle = function(userInput1, userInput2, userInput3) {
+  if ((userInput1 === userInput2) && (userInput1 === userInput3)){
+      return "equilateral";
+  } else if (((userInput1===userInput2) && (userInput1 !== userInput3)) ||
+        ((userInput1===userInput3) && (userInput1 !== userInput2)) ||
+        ((userInput2===userInput3) && (userInput2 !== userInput1))) {
+      return "isoscelese";
     } else {
-      return false;
-    }
-  var isoscelese = function() {
-    if (((userInput1+userInput2)%2===0) || ((userInput1+userInput3)%2===0) || ((userInput2+userInput3)%2===0))  {
-      alert("This is an isoscelese triangle.");
-    } else {
-      alert("This is an scalene triangle.");
+      return "scalene";
     }
   };
-  
-
-
-
-
-
-
 
 $(document).ready(function() {
   $("form").submit(function(event) {
   var userInput1 = parseInt($("input#side1").val());
       userInput2 = parseInt($("input#side2").val());
       userInput3 = parseInt($("input#side3").val());
-      input = userInput1 + userInput2 + userInput3;
 
 
-      $(".result").show();
 
 
    event.preventDefault();
